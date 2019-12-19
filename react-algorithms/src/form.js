ab6e29e2dc27578deb0d5bba79d099cff13d2f85
Zee Celest <zeecelest@gmail.com>
2019-12-18 19:33:33 -0500

    added handleSubmit button

--- react-algorithms/src/form.js
+++ react-algorithms/src/form.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import data from './data';
+import { render } from '@testing-library/react';
 
 class SearchForm extends React.Component {
     state = {
-        item: null,
+        input: null,
         located: false,
         count: 0
     };
@@ -13,18 +14,43 @@ class SearchForm extends React.Component {
         this.setState({sortedData})
     }
 
-    linearSearch(arr, item) {
+    linearSearch(arr, num) {
         console.log(arr);
         let counter = 0;
         for(let i = 0; i < arr.length; i++) {
             counter++;
-            if(arr[i] === item) {
+            if(arr[i] === input) {
                 return{
-                    item, located: true, counter
+                    input, located: true, counter
                 };
             }
         }
-        return {item, located: false, counter}
+        return {input, located: false, counter}
     };
 
 }
+    binarySearch(arr, item, start=0, end=arr.length - 1, counter=0) {
+        counter++;
+        it(start>end) {
+            return{input, located: false, counter};
+    }
+    const index= Math.floor((start+end)/2);
+    const item = arr[i];
+
+    console.log('index:' + index);
+    console.log('item:'+ item);
+
+    if(input === num) {
+        return{input, located: true, counter};
+    } else if (input > item) {
+        return this.binarySearch(arr, input, index+1, end, counter);
+    } else if (input < item) {
+        return this.binarySearch(arr, inpute, start, index-1, counter);
+    }
+}
+
+    render() {
+        return (
+            
+        )
+    }
